f97d84596911c75fe92ff8d03468a030a9d1f63e
TryHardDo <levai.levente2002@gmail.com>
2023-06-27 15:44:24 +0200

    Refactoring custom settings

---
+++ Settings/AppOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace TryInventories.Settings
+{
+    public class ProxyAccess
+    {
+        public string Username { get; set; } = "username";
+        public string Password { get; set; } = "password";
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    public class AppOptions
+    {
+        public const string Settings = "Settings";
+
+        public string ProxyHost { get; set; } = "http://something.com";
+        public int ProxyPort { get; set; } = 8080;
+        public bool UseAuthorization { get; set; } = true;
+        public ProxyAccess ProxyAccess { get; set; } = new ProxyAccess();
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
