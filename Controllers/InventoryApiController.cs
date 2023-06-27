978bb47d9aea6dd958a2cb4167af46d3a5d1f235
TryHardDo <levai.levente2002@gmail.com>
2023-06-27 16:01:40 +0200

    Adding MVC controller of the inventory endpoint

---
+++ Controllers/InventoryApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TryInventories.Controllers
+{
+    [ApiController]
+    [Route("inventory")]
+    public class InventoryApiController : Controller
+    {
+        [HttpGet("{steamId}")]
+        public ActionResult<object> GetInventory(string steamId)
+        {
+            return Ok();
+        }
+    }
+}
