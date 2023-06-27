f037b3d0b667cf7f0ead68e13119f3ca269cffa1
Lévai Levente <levai.levente2002@gmail.com>
2023-06-27 20:15:04 +0200

    Switching to Serilog as logging provider Adding logging middleware to the request pipeline

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
+using Serilog;
+using TryInventories.Middlewares;
 using TryInventories.Settings;
+
+namespace TryInventories;
 
 internal class Program
 {
+    internal const string Version = "v1.0.0";
+    internal const string Author = "Levai Levente @ TryHardDo";
+
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .WriteTo.Console()
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog(logger);
+
+        logger.Information("TryInventories - API provider");
+        logger.Information("Developed by: {Author} | Version: {Version}", Author, Version);
+        logger.Information("Project's source: https://github.com/TryHardDo/TryInventories");
+
         builder.Services.Configure<AppOptions>(builder.Configuration.GetSection(AppOptions.Settings));
+
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -18,6 +39,8 @@ internal class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.UseRequestLogger();
 
         app.UseHttpsRedirection();
 
