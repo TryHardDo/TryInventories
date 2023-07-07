87f9bcf92b7f6c61ae28851c3bfbb91926543932
Lévai Levente <levai.levente2002@gmail.com>
2023-07-07 22:06:52 +0200

    Renaming main class

--- TryInventories.cs
+++ TryInventories.cs
@@ -1,13 +1,16 @@
+using Microsoft.Extensions.Options;
 using Serilog;
 using TryInventories.Middlewares;
 using TryInventories.Settings;
+using TryInventories.Updater;
+using TryInventories.WebShareApi;
 
 namespace TryInventories;
 
-internal class Program
+internal class TryInventories
 {
-    internal const string Version = "v1.0.0";
     internal const string Author = "Levai Levente @ TryHardDo";
+    internal static readonly Version Version = new(1, 1, 0);
 
     private static void Main(string[] args)
     {
@@ -25,10 +28,32 @@ internal class Program
         logger.Information("TryInventories - API provider");
         logger.Information("Developed by: {Author} | Version: {Version}", Author, Version);
         logger.Information("Project's source: https://github.com/TryHardDo/TryInventories");
-
         builder.Services.Configure<AppOptions>(builder.Configuration.GetSection(AppOptions.Settings));
 
-        builder.Services.AddSingleton<SteamProxy>();
+        // WebShare client singleton service
+        builder.Services.AddSingleton<WebShareClient>();
+
+        // Version checker singleton service
+        builder.Services.AddSingleton(sp =>
+        {
+            var loggerService = sp.GetRequiredService<ILogger<VersionChecker>>();
+            var vc = new VersionChecker(loggerService);
+            vc.StartScheduledVersionChecker();
+
+            return vc;
+        });
+
+        // SteamProxy request middleman singleton service
+        builder.Services.AddSingleton(sp =>
+        {
+            var configuration = sp.GetRequiredService<IOptions<AppOptions>>();
+            var loggerService = sp.GetRequiredService<ILogger<SteamProxy>>();
+
+            var proxy = new SteamProxy(loggerService, configuration);
+            proxy.Init();
+
+            return proxy;
+        });
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -50,7 +75,9 @@ internal class Program
 
         app.MapControllers();
 
-        app.Services.GetService<SteamProxy>().Init(); // Other solution?
+        // Resolving services early to run init methods before app.Run()
+        app.Services.GetService<VersionChecker>();
+        app.Services.GetService<SteamProxy>();
 
         app.Run();
     }
