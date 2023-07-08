b83cd59b201d2079f76c17206359e58103126599
Lévai Levente <levai.levente2002@gmail.com>
2023-07-08 15:21:33 +0200

    Modifying settings structure

--- Settings/AppOptions.cs
+++ Settings/AppOptions.cs
@@ -13,16 +13,39 @@ public class ProxyAccess
     }
 }
 
+public class SelfRotatedProxySettings
+{
+    public string WebShareApiKey { get; set; } = string.Empty;
+    public int MaxRotatePerRequest { get; set; } = 20;
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
+
+public class AutoRotatedProxySettings
+{
+    public string ProxyHost { get; set; } = "p.webshare.io";
+    public int ProxyPort { get; set; } = 80;
+    public bool UseAuthorization { get; set; } = true;
+    public ProxyAccess AuthorizationCredentials { get; set; } = new();
+    public int MaxRetryPerRequest { get; set; } = 10;
+    public int RetryDelay { get; set; } = 1000;
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
+
 public class AppOptions
 {
     public const string Settings = "Settings";
 
     public bool SelfRotatedProxy { get; set; } = true;
-    public string WebShareApiKey { get; set; } = "webshare_api_key";
-    public string ProxyHost { get; set; } = "http://something.com";
-    public int ProxyPort { get; set; } = 8080;
-    public bool UseAuthorization { get; set; } = true;
-    public ProxyAccess ProxyAccess { get; set; } = new();
+    public SelfRotatedProxySettings SelfRotatedProxySettings { get; set; } = new();
+    public AutoRotatedProxySettings AutoRotatedProxySettings { get; set; } = new();
     public string AccessKey { get; set; } = "place_some_api_key_here";
 
     public override string ToString()
